a9df7ee81ac192631b9d2fc54d0ea63dc2e98649
pawelsoczowka <soczowka.pawel91@gmail.com>
2018-10-08 20:30:56 +0200

    add CSVReader, outcome printed to console

---
+++ src/main/java/com/github/kukisnake/reader/QuestionsAndAnswers.java
@@ -0,0 +1,66 @@
+package com.github.kukisnake.reader;
+
+public class QuestionsAndAnswers {
+
+    private String question;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+
+    public QuestionsAndAnswers(String question, String answer1, String answer2, String answer3, String answer4) {
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.answer4 = answer4;
+        this.question = question;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer1() {
+        return answer1;
+    }
+
+    public void setAnswer1(String answer1) {
+        this.answer1 = answer1;
+    }
+
+    public String getAnswer2() {
+        return answer2;
+    }
+
+    public void setAnswer2(String answer2) {
+        this.answer2 = answer2;
+    }
+
+    public String getAnswer3() {
+        return answer3;
+    }
+
+    public void setAnswer3(String answer3) {
+        this.answer3 = answer3;
+    }
+
+    public String getAnswer4() {
+        return answer4;
+    }
+
+    public void setAnswer4(String answer4) {
+        this.answer4 = answer4;
+    }
+
+    @Override
+    public String toString() {
+        return "Question: " + question + " \n A: " + answer1 +
+                "\n B: " + answer2 +
+                "\n C: " + answer3 +
+                "\n D: " + answer4;
+    }
+}
